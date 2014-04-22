d9cfef4881ba819c82db9430efcdc2b5bcb18ef8
Silas Poulsen <sillas_2@hotmail.com>
2014-04-22 23:12:06 +0200

    implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Coderstravel::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # requiered for heroku
+  # note to set this to actual host 
+  config.action_mailer.default_url_options = { :host => 'coderstravel.heroku.com' }
 end
